bf91b3fbf12a0b60808f3923dcf69dd36bd83d65
Milan Suk <Milansuk@email.cz>
2017-09-17 13:15:00 +0200

    trash

--- tcp_server.py
+++ tcp_server.py
@@ -1,4 +1,5 @@
 import socket
+import io
 from PIL import Image
 
 def run():
@@ -6,22 +7,27 @@ def run():
     run TCP server
     :return:
     """
-    TCP_IP = "192.168.0.158"
+    TCP_IP = "0.0.0.0"
     TCP_PORT = 9090
-    BUFFER_SIZE = 100
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((TCP_IP, TCP_PORT))
     s.listen(True)
 
-    while (True):
-        print(" --- new connection")
-        conn, addr = s.accept()
-        length = recvall(conn, 16)
+    conn, addr = s.accept()
+    length = conn.recv(4)
 
-        stringData = recvall(conn, int(length))
-        data = Image.fromstring(stringData, dtype='uint8')
+    print("new connection")
 
-        file = "./unknown/{}.jpg".format(len([name for name in os.listdir(DIR)
-                                              if os.path.isfile(os.path.join(DIR, name))]))
-        data.save(file);
+    size = ord(length[0]) * 16777216 + ord(length[1]) * 65536 + ord(length[2]) * 256 + ord(length[3])
+
+    print "size: {}".format(size)
+
+    stringData = conn.recv(size)
+    data = Image.open(io.BytesIO(stringData))
+
+    file = "./unknown/{}.jpg".format(len([name for name in os.listdir(DIR)
+        if os.path.isfile(os.path.join(DIR, name))]))
+    data.save(file);
+
+    s.close()
