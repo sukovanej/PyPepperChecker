d687e0f1d9752c1205f98ae36b56ce199af6c667
sukovanej <suk@eurosat.cz>
2017-09-17 15:15:59 +0200

    run neural network

--- __init__.py
+++ __init__.py
@@ -11,11 +11,11 @@ INPUT_SIZE = 172 * 229 * 3
 print("Building neural network...")
 network = Network(INPUT_SIZE, OUTPUT_SIZE)
 
-# print "Processing images samples..."
-# network.process_images()
+print "Processing images samples..."
+network.process_images()
 
-# print "Training network..."
-# network.train()
+print "Training network..."
+network.train()
 
 print("Running TCP server...")
 tcp_server = TcpServer(network)
